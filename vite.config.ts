919df480df0518b04a1b2df68de22867c9691469
neibips <68771174+neibips@users.noreply.github.com>
2025-02-17 11:41:51 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
@@ -7,5 +8,12 @@ export default defineConfig({
   plugins: [
       react(),
       tailwindcss(),
+      checker({
+          typescript: true, // Enable TypeScript checking
+          overlay: true,    // Show errors in the browser overlay
+          eslint: { lintCommand: 'eslint src' }, // Optional: Add ESLint checking
+          // Disable failing the build on errors
+          silent: true,     // Suppress terminal output for type errors
+      }),
   ],
 })
